753acb55e3d592b5bc9f713b4c0ebc0717dde683
maximk <kuleshov.max.v@gmail.com>
2014-03-06 15:43:48 +0400

    CEL platform

--- geo.py
+++ geo.py
@@ -2,9 +2,22 @@ __author__ = 'maximk'
 
 from Bio import Entrez
 import os
-os.environ["http_proxy"] = "187.72.124.66:8080"
+from ftplib import FTP
+
+os.environ["http_proxy"] = "192.3.25.99:3127"
 
 Entrez.email = 'kuleshov.max.v@gmail.com'
+
+def cel(ftp_adress):
+    ftp = FTP(ftp_adress)
+    ftp.login()
+    files_list = ftp.nlst()
+    return None
+
+def platform(gpl):
+    handle = Entrez.esearch(db="gds", term= ''.join([gpl, '[GEO Accession]']))
+    summary = Entrez.read(handle)
+    return None
 
 def retrieve_record(query):
     '''
@@ -14,9 +27,14 @@ def retrieve_record(query):
 
     record = Entrez.read(handle)
     for geo_id in record['IdList']:
-            handle = Entrez.esummary(db="gds", id=geo_id)
+            handle = Entrez.esummary(db='gds', id=geo_id)
             summary = Entrez.read(handle)
+            #cel_presence = cel(summary[0]['FTPLink'])
+            platform(summary[0]['GPL'])
             print('GEO Series ID: %s\nName: %s\n%s samples\nGEO Platform ID: GPL%s' % (summary[0]['Accession'], summary[0]['title'], len(summary[0]['Samples']), summary[0]['GPL']))
             print()
 
     return None
+
+retrieve_record('')
+#cel('ftp.debian.org')
