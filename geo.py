6e83127c6d89a0f00baab696da05b2f3f9ad8185
maximk <kuleshov.max.v@gmail.com>
2014-03-06 15:50:45 +0400

    CEL platform

--- geo.py
+++ geo.py
@@ -9,20 +9,26 @@ os.environ["http_proxy"] = "192.3.25.99:3127"
 Entrez.email = 'kuleshov.max.v@gmail.com'
 
 def cel(ftp_adress):
+    """
+    Проверяет наличие CEL-файлов
+    """
     ftp = FTP(ftp_adress)
     ftp.login()
     files_list = ftp.nlst()
     return None
 
 def platform(gpl):
+    """
+    Возвращает название платформы
+    """
     handle = Entrez.esearch(db="gds", term= ''.join([gpl, '[GEO Accession]']))
     summary = Entrez.read(handle)
     return None
 
 def retrieve_record(query):
-    '''
+    """
     Возвращает выбранные параметры для всех записией по данному запросу
-    '''
+    """
     handle = Entrez.esearch(db='gds', retmax=200, term='("bevacizumab"[All Fields] OR Bevacizumab[All Fields]) AND ("expression profiling by array"[DataSet Type] OR "expression profiling by high throughput sequencing"[DataSet Type]) AND "gse"[Filter]')
 
     record = Entrez.read(handle)
