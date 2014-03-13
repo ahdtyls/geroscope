7089ec17cdc9fd0ecede1ed334a87fd53315cce8
maximk <kuleshov.max.v@gmail.com>
2014-03-13 17:38:13 +0400

    + pickle gero_dict processed

--- geo.py
+++ geo.py
@@ -89,7 +89,7 @@ def retrieve_record(gero_dict):
                             file = open('/home/maximk/Work/geroscope/gero.txt', 'a')
                             file.write('%s;%s;%s;%s;%s;%s;GPL%s;%s\n' % (drug, alias, summary[0]['Accession'], summary[0]['title'], summary[0]['n_samples'], cel_presence, c, ','.join(platform(c))))
                             file.close()
-                            gero_dict_copy[drug][alias].pop(geo_id)
+                            gero_dict_copy[drug][alias].pop(str(geo_id))
         with open('/home/maximk/Work/geroscope/gero_dict_unprocess.pickle', 'wb') as f:
             pickle.dump(gero_dict, f)
 
