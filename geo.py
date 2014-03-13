3ddf24ddb0e6bfb3c13c9b8cf2358cf61b999aa6
maximk <kuleshov.max.v@gmail.com>
2014-03-13 19:02:48 +0400

    + pickle gero_dict processed

--- geo.py
+++ geo.py
@@ -86,10 +86,10 @@ def retrieve_record(gero_dict):
                         cel_presence = cel(summary[0]['FTPLink'])
                         for c in str(summary[0]['GPL']).split(sep = ';'):
                             print('%s;%s;%s;%s;%s;%s;GPL%s;%s' % (drug, alias, summary[0]['Accession'], summary[0]['title'], summary[0]['n_samples'], cel_presence, c, ','.join(platform(c))))
-                            file = open('/home/maximk/Work/geroscope/gero.txt', 'a')
-                            file.write('%s;%s;%s;%s;%s;%s;GPL%s;%s\n' % (drug, alias, summary[0]['Accession'], summary[0]['title'], summary[0]['n_samples'], cel_presence, c, ','.join(platform(c))))
-                            file.close()
-                            gero_dict_copy[drug][alias].pop(str(geo_id))
+                            with open('/home/maximk/Work/geroscope/gero.txt', 'a') as file:
+                                file.write('%s;%s;%s;%s;%s;%s;GPL%s;%s\n' % (drug, alias, summary[0]['Accession'], summary[0]['title'], summary[0]['n_samples'], cel_presence, c, ','.join(platform(c))))
+                            if(gero_dict_copy[drug][alias])and(geo_id in gero_dict_copy[drug][alias]):
+                                gero_dict_copy[drug][alias].remove(geo_id)
         with open('/home/maximk/Work/geroscope/gero_dict_unprocess.pickle', 'wb') as f:
             pickle.dump(gero_dict, f)
 
