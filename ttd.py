fbc9caa62e46c669acda4ac5b16dfdd3fe381af8
maxim-k <kuleshov.max.v@gmail.com>
2014-04-16 17:20:31 +0400

    TTD

--- ttd.py
+++ ttd.py
@@ -1,34 +1,41 @@
 __author__ = 'maximk'
 
-def add_to_dic(dic, key, element):
-    if key in dic:
-        dic[key]
-    else:
-        dic[key] = element
+
+# def add_to_dic(dic, key, element):
+#     if key in dic:
+#         dic[key]
+#     else:
+#         dic[key] = element
+
 
 def ttd_uniprot(file):
-    uniprot = [line.split(sep='\t') for line in file.read().split(sep='\n')]
+    uniprot = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
     ids_set = set(line[0] for line in uniprot)
     keys_set = set(line[0] for line in uniprot)
     return None
 
 
 def ttd_syn(file):
-    syn = [line.split(sep='\t') for line in file.read().split(sep='\n')]
+    syn = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
     return None
 
 
 def ttd_crossmatch(file):
-    crossmatch = [line.split(sep='\t') for line in file.read().split(sep='\n')]
+    crossmatch = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
     ids_set = set(line[0] for line in crossmatch)
     keys_set = set(line[0] for line in crossmatch)
     return None
 
 
 def ttd_main(file):
-    main = [line.split(sep='\t') for line in file.read().split(sep='\n')]
+    main_dict = dict()
+    main = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
     ids_set = sorted(list(set(line[0] for line in main)))
     keys_set = set(line[1] for line in main)
+    for main_id in ids_set:
+        main_dict[main_id] = dict(zip(keys_set, ['']*len(keys_set)))
+    for line in main:
+        main_dict[line[0]][line[1]] = line[2]
     return None
 
 
