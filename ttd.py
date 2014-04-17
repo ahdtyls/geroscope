9c858ba1f8debf65adacd45afe721e41b3274e43
maxim-k <kuleshov.max.v@gmail.com>
2014-04-17 14:41:13 +0400

    TTD+

--- ttd.py
+++ ttd.py
@@ -1,44 +1,30 @@
 __author__ = 'maximk'
 
 
-# def add_to_dic(dic, key, element):
-#     if key in dic:
-#         dic[key]
-#     else:
-#         dic[key] = element
-
-
-def ttd_uniprot(file):
-    uniprot = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
-    ids_set = set(line[0] for line in uniprot)
-    keys_set = set(line[0] for line in uniprot)
-    return None
-
-
-def ttd_syn(file):
-    syn = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
-    return None
-
-
-def ttd_crossmatch(file):
-    crossmatch = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
-    ids_set = set(line[0] for line in crossmatch)
-    keys_set = set(line[0] for line in crossmatch)
-    return None
-
-
 def ttd_main(file):
     main_dict = dict()
-    main = [line.replace('\ufeff', '').split(sep='\t') for line in file.read().split(sep='\n')]
+    main = [line.split(sep='\t') for line in file.read().replace('\ufeff', '').split(sep='\n')]
     ids_set = sorted(list(set(line[0] for line in main)))
     keys_set = set(line[1] for line in main)
     for main_id in ids_set:
         main_dict[main_id] = dict(zip(keys_set, ['']*len(keys_set)))
     for line in main:
         main_dict[line[0]][line[1]] = line[2]
-    return None
+    return main_dict
 
 
+ttd = ttd_main(open('/home/maximk/Work/geroscope/ttd/TTD_download.txt', 'r'))
+result = []
+actions = sorted(['Activator', 'Intercalator', 'Ligand', 'Antibody', 'Aptamer', 'Opener', 'Blocker', 'Antagonist',
+           'Suppressor', 'Breaker', 'Regulator', 'Agonist', 'Antisense', 'Enhancer', 'Minor groove binder',
+           'Antigen', 'Vaccine', 'Stimulator', 'Cofactor', 'Binder', 'Stablizer', 'Modulator', 'Inducer',
+           'Inhibitor', 'UpRegulator', 'Adduct'])
 
+for rec in ttd.values():
+    target = '%s\t%s\t%s\t%s\n' % (rec['Name'], rec['Synonyms'], rec['UniProt ID'], rec['Type of target'])
+    for action in actions:
+        if rec[action]:
+            result.append('%s\t%s\t%s' % (rec[action], action, target))
 
-ttd_main(open('/home/maximk/Work/geroscope/ttd/TTD_download.txt', 'r'))
+with open('/home/maximk/Work/geroscope/ttd/ttd.tsv', 'w') as ttd_result:
+    ttd_result.writelines(sorted(result))
