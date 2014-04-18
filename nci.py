b83cfa23573e5720366c04955cb21f20c9f27e06
maxim-k <kuleshov.max.v@gmail.com>
2014-04-18 19:54:05 +0400

    Pickle, sorting, merging

--- nci.py
+++ nci.py
@@ -1,46 +1,70 @@
 __author__ = 'maximk'
 
 import xmltodict
+import os.path
+import pickle
+from collections import OrderedDict
 
-nci = xmltodict.parse(open('/home/maximk/Work/geroscope/nci_cancerindex/nci.xml', 'r').read())
+if os.path.isfile('/home/maximk/Work/geroscope/nci_cancerindex/nci.pickle'):
+    with open('/home/maximk/Work/geroscope/nci_cancerindex/nci.pickle', 'rb') as nci_pickle:
+        nci = pickle.load(nci_pickle)
+else:
+    nci = xmltodict.parse(open('/home/maximk/Work/geroscope/nci_cancerindex/nci.xml', 'r').read())
+    with open('/home/maximk/Work/geroscope/nci_cancerindex/nci.pickle', 'wb') as nci_pickle:
+        pickle.dump(nci, nci_pickle)
 
-for gene_entry in nci['GeneEntryCollection']['GeneEntry']:
-    gene_name = gene_entry.get('HUGOGeneSymbol', '')
-    if gene_name == 'ACSL5':
-        print()
-    gene_genbank = gene_entry.get('GenbankAccession', '')
-    gene_refseq = gene_entry.get('RefSeqID', '')
-    gene_uniprot = gene_entry.get('UniProtID', '')
-    if type(gene_entry.get('Sentence', '')) == str:
-        print()
-    if gene_entry.get('Sentence', ''):
-        for sentence in gene_entry['Sentence']:
-            if sentence.get('Organism', '') == 'Human':
-                cell_line = sentence.get('CellineIndicator', '')
-                evidence = sentence.get('EvidenceCode', '')
-                if type(evidence) == list:
-                    evidence = ', '.join(evidence)
+nci_result = list()
+for gene_entry_key in nci['GeneEntryCollection'].keys():
+    nci_set = set()
+    if gene_entry_key == 'GeneEntry':
+        gene_entry_list = nci['GeneEntryCollection']['GeneEntry']
+        if type(gene_entry_list) == OrderedDict:
+            gene_entry_list = [gene_entry_list]
+        for gene_entry in gene_entry_list:
+            gene_name = gene_entry.get('HUGOGeneSymbol', '')
+            if gene_name == 'ACSL5':
+                print()
+            gene_genbank = gene_entry.get('GenbankAccession', '')
+            gene_refseq = gene_entry.get('RefSeqID', '')
+            gene_uniprot = gene_entry.get('UniProtID', '')
 
-                drug_name = ''
-                drug_concept = ''
-                if sentence.get('DrugData', ''):
-                    drug_name = sentence['DrugData'].get('DrugTerm', '')
-                    drug_concept = sentence['DrugData'].get('NCIDrugConceptCode', '')
+            if gene_entry.get('Sentence', ''):
+                for sentence_key in gene_entry.keys():
+                    if sentence_key == 'Sentence':
+                        sentence_list = gene_entry['Sentence']
+                        if type(sentence_list) == OrderedDict:
+                            sentence_list = [sentence_list]
+                        for sentence in sentence_list:
+                            if sentence.get('Organism', '') == 'Human':
+                                cell_line = sentence.get('CellineIndicator', '')
+                                evidence = sentence.get('EvidenceCode', '')
+                                if type(evidence) == list:
+                                    evidence = ', '.join(evidence)
 
-                gene_concept = ''
-                if sentence.get('GeneData', ''):
-                    gene_concept = sentence['GeneData'].get('NCIGeneConceptCode', '')
+                                drug_name = ''
+                                drug_concept = ''
+                                if sentence.get('DrugData', ''):
+                                    drug_name = sentence['DrugData'].get('DrugTerm', '')
+                                    drug_concept = sentence['DrugData'].get('NCIDrugConceptCode', '')
 
-                role = ''
-                if sentence.get('Roles', ''):
-                    role = sentence['Roles'].get('PrimaryNCIRoleCode', '')
-                    if type(role) == list:
-                        role = ', '.join(role)
-                    role = role.replace('_', ' ')
+                                gene_concept = ''
+                                if sentence.get('GeneData', ''):
+                                    gene_concept = sentence['GeneData'].get('NCIGeneConceptCode', '')
 
-                sentence_status = sentence.get('SentenceStatusFlag').replace('_', ' ')
+                                role = ''
+                                if sentence.get('Roles', ''):
+                                    role = sentence['Roles'].get('PrimaryNCIRoleCode', '')
+                                    if type(role) == list:
+                                        role = ', '.join(role)
+                                    role = role.replace('_', ' ')
 
-                with open('/home/maximk/Work/geroscope/nci_cancerindex/nci.tsv', 'a') as nci_result:
-                    nci_result.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'
-                                     % (drug_name, drug_concept, gene_name, gene_concept, role,
-                                        gene_genbank, gene_refseq, gene_uniprot, cell_line, evidence, sentence_status))
+                                sentence_status = sentence.get('SentenceStatusFlag').replace('_', ' ')
+
+                                nci_set.add('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'
+                                                     % (drug_name, drug_concept, gene_name, gene_concept, role,
+                                                        gene_genbank, gene_refseq, gene_uniprot, cell_line, evidence,
+                                                        sentence_status))
+    nci_result += list(nci_set)
+
+with open('/home/maximk/Work/geroscope/nci_cancerindex/nci.tsv', 'a') as nci_result_file:
+    nci_result_file.writelines(sorted(nci_result))
