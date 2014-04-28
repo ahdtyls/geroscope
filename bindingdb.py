90669ed4b4989279d3d8fcbc55e1876573bb8a68
maxim-k <kuleshov.max.v@gmail.com>
2014-04-28 18:31:47 +0400

    BindingDB extend

--- bindingdb.py
+++ bindingdb.py
@@ -6,5 +6,21 @@ with open('/home/maximk/Work/geroscope/bindingdb/bindingdb.tsv', 'a') as binding
     for line in binding_file.read().strip().split(sep='\n'):
         rec = line.split('\t')
         if 'Homo sapiens' in rec[5]:
-            # Drug name, Target name, Uniprot Gene Name, Source,
-            bindingdb.write('%s\t%s\t%s\t%s\n' % (rec[3], rec[4], rec[38].split(sep='_')[0], rec[14]))
+            drug_name = rec[3]
+            pubchem_cid = rec[26]
+            pubchem_sid = rec[27]
+            chebi = rec[28]
+            chembl = rec[29]
+            kegg = rec[32]
+
+            action = 'binding'
+            evidence = rec[14]
+            pmid = rec[16]
+
+            target_name = rec[4]
+            gene_symbol = rec[38].split(sep='_')[0]
+            uniprot = rec[38]
+
+            bindingdb.write('%s\t\t\t\t\t%s\t%s\t%s\t%s\t%s\t%s\t\t%s\t%s\t\t%s\t%s\t\t\t%s\t\n'
+                            % (drug_name, pubchem_cid, pubchem_sid, chebi, chembl, kegg, action, evidence, pmid,
+                               gene_symbol,target_name, uniprot))
