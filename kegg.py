f434d8391586c4549508d4e7533185201f3fdc0b
maxim-k <kuleshov.max.v@gmail.com>
2014-05-06 16:06:18 +0400

    KEGG HSA UniProt, ENSG

--- kegg.py
+++ kegg.py
@@ -3,7 +3,6 @@ __author__ = 'maximk'
 import urllib.request
 from tissues import retry
 
-
 @retry(urllib.error.URLError)
 def get_target_genes(target_ids):
     """
@@ -11,6 +10,8 @@ def get_target_genes(target_ids):
     :param target_ids: TARGET string from KEGG Drugs
     """
     hsa_names = []
+    uniprot = []
+    ensg = []
 
     target_tails = list()
     for line in target_ids:
@@ -20,13 +21,25 @@ def get_target_genes(target_ids):
     for target_id in target_tails:
         url = 'http://www.kegg.jp/dbget-bin/www_bget?%s' % target_id
         kegg_html = urllib.request.urlopen(url).read().decode('utf-8').split(sep='\n')
-        for line in range(len(kegg_html)):
-            if ('<nobr>Name</nobr>' in kegg_html[line]) or ('<nobr>Gene name</nobr>' in kegg_html[line]):
-                names = kegg_html[line + 1].split(sep='>')[-2].replace('<br', '')
-                if 'hsa' in target_id:
-                    hsa_names.append(names)
 
-    return ', '.join(hsa_names)
+        for line in range(len(kegg_html)):
+            if '<nobr>Gene name</nobr>' in kegg_html[line]:
+                names = kegg_html[line + 1].split(sep='>')[-2].replace('<br', '')
+                hsa_names.append(names)
+            elif('Other DBs' in kegg_html[line]):
+                clob = kegg_html[line+1].split(sep='</div>')
+                for db_line in clob:
+                    if 'ensembl' in db_line:
+                        ensg_line = db_line.replace('<div style="margin-left:5em">', '')\
+                            .replace('<a href="http://www.ensembl.org/Homo_sapiens/geneview?gene=', '')\
+                            .replace('">', ' ').replace('</a>', ' ')
+                        ensg.append(list((set(ensg_line.strip().split()))))
+                    elif 'uniprot' in db_line:
+                        uniprot_line = db_line.replace('<div style="margin-left:5em">', '')\
+                            .replace('<a href="http://www.uniprot.org/uniprot/', '')\
+                            .replace('">', ' ').replace('</a>', ' ')
+                        uniprot.append(list((set(uniprot_line.strip().split()))))
+    return {'gene_symbol': ', '.join(hsa_names), 'uniprot': ', '.join(uniprot), 'ensg': ', '.join(ensg)}
 
 
 with open('/home/maximk/Work/geroscope/kegg/drug.db', 'r') as kegg_db:
@@ -81,15 +94,12 @@ for drug in kegg_keys:
                 elif link.split(sep=':')[0].strip() == 'CAS':
                     cas = link.split(sep=':')[1].strip()
 
-
-
-
             target_na = []
             for target in kegg_dict[drug]['TARGET'].split(sep='\n'):
                 target_name = ' '.join(target.split(sep='[')[0].strip().split(sep=' ')[:-1])
                 target_action = target.split(sep='[')[0].strip().split(sep=' ')[-1]
                 target_genes = get_target_genes(target.split(sep='['))
-                target_na.append('%s\t\t\t\t\t%s\t%s\n' % (target_action, target_genes, target_name))
+                target_na.append('%s\t\t\t\t\t%s\t%s\t\t%s\t%s\n' % (target_action, target_genes['gene_symbol'], target_name, target_genes['ensg'], target_genes['uniprot']))
             for target_rec in target_na:
                 with open('/home/maximk/Work/geroscope/kegg/kegg.csv', 'a') as kegg_file:
                     kegg_file.write('%s\t%s\t%s\t%s\t\t\t%s\t\t\t%s\t%s' % (name, alias, atc, cas, pubchem_id, drug, target_rec))
