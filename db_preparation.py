2eaaeee2a423712e4c57d2989c7febb8ca2daeda
maxim-k <kuleshov.max.v@gmail.com>
2014-05-13 15:46:24 +0400

    Empty 'drugbank' field added

--- db_preparation.py
+++ db_preparation.py
@@ -12,10 +12,12 @@ db_prefix = {'bindingdb_dtdb.tsv': 'BDB', 'chembl_dtdb.tsv': 'CBL', 'ctd_dtdb.ts
              'nci_dtdb.tsv': 'NCI', 'pgkb_dtdb.tsv': 'PGK', 'stitch_dtdb.tsv': 'STI',
              'ttd_dtdb.tsv': 'TTD'}
 
-for filename in filenames:
-    with open('dtdb_path'+filename, 'r') as file:
+for filename in sorted(filenames):
+    with open(dtdb_path+filename, 'r') as file:
         prefix = db_prefix[filename]
         for line_num, line in enumerate(file):
+            if (prefix == 'KEG')or(prefix == 'TTD'):
+                line = line.replace('\n', '\t\n')
             num = '0'*(7-len(str(line_num))) + str(line_num)
             if line_num > 0:
                 line_split = line.split(sep='\t')
@@ -23,6 +25,6 @@ for filename in filenames:
                 action = open('/home/maximk/Work/geroscope/dtdb/action.tsv', 'a')
                 target = open('/home/maximk/Work/geroscope/dtdb/target.tsv', 'a')
 
-                drug.write('%sD%s\t%s\t\n' % (prefix, num, '\t'.join(line[0:10])))
-                action.write('%sA%s\t%sD%s\t%sT%s\t%s\t\n' % (prefix, num, prefix, num, prefix, num, '\t'.join(line[10:15])))
-                target.write('%sT%s\t%s\n' % (prefix, num, '\t'.join(line[15:])))
+                drug.write('%sD%s\t%s\t\n' % (prefix, num, '\t'.join(line_split[0:10])))
+                action.write('%sA%s\t%sD%s\t%sT%s\t%s\t%s\n' % (prefix, num, prefix, num, prefix, num, '\t'.join(line_split[10:15]), prefix))
+                target.write(('%sT%s\t%s' % (prefix, num, '\t'.join(line_split[15:]))).replace('\n','\t\n'))
